70adaf9fc967276420a93fd68cf79bc5abec55ab
sergiorturizo <tsergior@gmail.com>
2025-02-25 15:55:44 -0500

    Update cors

--- src/main.ts
+++ src/main.ts
@@ -3,23 +3,36 @@ import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import * as express from 'express';
 import * as serverless from 'serverless-http';
-import { AllExceptionsFilter } from './common/all-exceptions.fIlter';
+import { AllExceptionsFilter } from './common/all-exceptions.filter';
 
 const server = express();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.setGlobalPrefix('api/v1');
+
   app.enableCors({
-    origin: ['https://iris-app-front-sergio.netlify.app'],
+    origin: (origin, callback) => {
+      if (!origin || origin.includes('netlify.app')) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
   });
+
   app.useGlobalFilters(new AllExceptionsFilter());
+
   await app.init();
 }
 
 bootstrap();
 
-export const handler = serverless(server);
+export const handler = serverless(server, {
+  request: (req, event) => {
+    req.requestContext = event.requestContext;
+  },
+});
